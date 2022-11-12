67f6ebe507f1cadae1364fed6bd41b2c3dee9fb5
Burhan Mafazi <burhanburdev@gmail.com>
2022-11-13 02:13:49 +0700

    update build hold drop

--- app/Http/Controllers/BuildHoldDropController.php
+++ app/Http/Controllers/BuildHoldDropController.php
@@ -8,8 +8,179 @@ use Illuminate\Support\Facades\Validator;
 class BuildHoldDropController extends Controller
 {
     public function index(Request $request)
-    {
-        
+    {   
+        $depth = [];
+
+        $mdChartValue = [];
+        $tvdChartValue = [];
+
+        $eob_md = 0;
+        $eob_vd = 0;
+        $eob_displacement = 0;
+
+        $sod_md = 0;
+        $sod_vd = 0;
+        $sod_displacement = 0;
+
+        $eod_md = 0;
+        $eod_vd = 0;
+        $eod_displacement = 0;
+
+        $kop = $request->input('kop'); // kick of point
+        $bur = $request->input('bur'); // build up rate
+        $dor = $request->input('dor'); // drop off rate
+
+        $target = $request->input('target'); // target end of drop
+        $n = $request->input('n'); // northing
+        $e = $request->input('e'); // easting
+
+        if ($kop && $bur && $dor && $target && $n && $e) {
+            $pi = pi();
+
+            $horizontal_displacement = pow((pow($n, 2) + pow($e, 2)), 0.5);
+            $radius_curvature_bur = (180 * 100) / ($bur * $pi);
+            $radius_curvature_dor = (180 * 100) / ($dor * $pi);
+
+            $sudut_B = 0;
+            $line_OF = 0;
+            $line_OG = 0;
+            $sudut_FOG = 0;
+            $max_inclination = 0;
+
+            $eod_vd = (double) $target;
+
+            if ($radius_curvature_bur + $radius_curvature_dor <= $horizontal_displacement) {
+                $line_x = $horizontal_displacement - ($radius_curvature_bur + $radius_curvature_dor);
+                $sudut_B = rad2deg(tan($line_x / ($target - $kop)));
+                $line_OF = ($target - $kop) / cos(deg2rad($sudut_B));
+                $line_OG = pow(pow($line_OF, 2) - pow(($radius_curvature_bur + $radius_curvature_dor), 2), 0.5);
+                $sudut_FOG = rad2deg(asin(($radius_curvature_bur + $radius_curvature_dor) / $line_OF));
+                $max_inclination = $sudut_FOG + $sudut_B;
+                
+                // eob
+                $eob_md = $kop + (($max_inclination * 100) / $bur);
+                $eob_vd = $kop + ($radius_curvature_bur * sin(deg2rad($max_inclination)));
+                $eob_displacement = $radius_curvature_bur * (1 - cos(deg2rad($max_inclination)));
+
+                // sod
+                $sod_displacement = $horizontal_displacement - ($radius_curvature_dor * (1 - cos(deg2rad($max_inclination))));
+                $sod_md = $eob_md + $line_OG;
+                $sod_vd = $eob_vd + ($line_OG * cos(deg2rad($max_inclination)));
+
+                // eod
+                $eod_md = $sod_md + (($max_inclination * 100) / $dor);
+                $eod_displacement = $sod_displacement + ($radius_curvature_dor * (1 - cos(deg2rad($max_inclination))));
+            } else {
+                $line_x = rad2deg(
+                    (
+                        atan(
+                            ($target - $kop) / ($radius_curvature_bur + $radius_curvature_dor) - $horizontal_displacement
+                        )
+                    ) - (
+                        acos(
+                            (
+                                $radius_curvature_bur + $radius_curvature_dor) / ($target - $kop) * sin(
+                                atan(
+                                    ($radius_curvature_bur + $radius_curvature_dor) / (($target - $kop) - $horizontal_displacement)
+                                )
+                            )
+                        )
+                    )
+                );
+
+                // eob
+                $eob_md = $kop + (($line_x * 100) / $bur);;
+                $eob_vd = $kop + ($radius_curvature_bur * sin(deg2rad($line_x)));
+                $eob_displacement = $radius_curvature_bur * (1 - cos(deg2rad($line_x)));
+
+                // sod
+                $sod_displacement = $horizontal_displacement - ($radius_curvature_dor * (1 - cos(deg2rad($line_x))));
+                $sod_md = $eob_md + (($sod_displacement - $eob_displacement) / (sin(deg2rad($line_x))));
+                $sod_vd = $target - ($radius_curvature_dor * (1 - sin(deg2rad($line_x))));
+
+                // eod
+                $eod_md = $sod_md + (($pi / 180) * $radius_curvature_dor * $line_x);
+                $eod_displacement = $radius_curvature_dor * (1 - cos(deg2rad($line_x))) + $sod_displacement;
+            }
+
+            $inc = 1;
+            $inclination = 0;
+
+            // vertical
+            for ($i=0; $i <= $kop; $i+=100) {
+                if ($i == $kop) {
+                    $status = 'KOP';
+                } else {
+                    $status = 'Vertical';
+                }
+
+                $total_departure = 0;
+
+                $depth[$inc]['md'] = $i;
+                $depth[$inc]['inclination'] = $inclination;
+                $depth[$inc]['tvd'] = $i;
+                $depth[$inc]['total_departure'] = $total_departure;
+                $depth[$inc]['status'] = $status;
+
+                $inc++;
+            }
+
+            $tvdKOP = $i-100;
+
+            $mdChartValue[] = $total_departure;
+            $tvdChartValue[] = $i-100;
+
+            // build
+            for ($i = $i; $i <= $eob_md; $i+=100) {
+                $status = 'Build';
+
+                // find inclination
+                if ($i > $kop && $eob_md > $i) {
+                    $inclination = $bur + $inclination;
+                }
+
+                // find tvd
+                $tvd = $radius_curvature_bur * sin(deg2rad($inclination)) + $tvdKOP;
+
+                // find total_departure
+                $total_departure = $radius_curvature_bur * (1 - cos(deg2rad($inclination)));
+
+                $depth[$inc]['md'] = $i;
+                $depth[$inc]['inclination'] = $inclination;
+                $depth[$inc]['tvd'] = $tvd;
+                $depth[$inc]['total_departure'] = $total_departure;
+                $depth[$inc]['status'] = $status;
+
+                $mdChartValue[] = round($total_departure, 2);
+                $tvdChartValue[] = $tvd;
+
+                $inc++;
+            }
+
+            // end of build
+            $status = 'End of Build';
+            $inclinationEOB = ($eob_md - ($i - 100)) * ($bur / 100) + $inclination;
+            $tvdEOB = ($radius_curvature_bur * sin(deg2rad($inclinationEOB))) + $tvdKOP;
+            $total_departureEOB = $radius_curvature_bur * (1 - cos(deg2rad($inclinationEOB)));
+
+            $depth[$inc+1]['md'] = $eob_md;
+            $depth[$inc+1]['inclination'] = $inclinationEOB;
+            $depth[$inc+1]['tvd'] = $tvdEOB;
+            $depth[$inc+1]['total_departure'] = $total_departureEOB;
+            $depth[$inc+1]['status'] = $status;
+
+            $mdChartValue[] = round($total_departureEOB, 2);
+            $tvdChartValue[] = $tvdEOB;
+
+            $inc++;
+        }
+
+        // echo "<pre>";
+        // var_dump($eod_md);
+        // var_dump($eod_vd);
+        // var_dump($eod_displacement);
+        // die();
+
         return view('build-hold-drop', get_defined_vars());
     }
 }
--- resources/views/build-hold-drop.blade.php
+++ resources/views/build-hold-drop.blade.php
@@ -7,20 +7,194 @@
     .text-center {
         text-align: center;
     }
+
+    .form-control-custom {
+        width: 70%;
+        height: calc(1.5em + 0.75rem + 2px);
+        padding: 0.375rem 0.75rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #495057;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #ced4da;
+        border-radius: 0.25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
 </style>
 @endsection
 
 @section('js')
+<script>
+    var xValues = <?= json_encode($mdChartValue) ?>; // md
+    var yValues = <?= json_encode($tvdChartValue) ?>; // tvd
+
+    var myChart = new Chart("myChart", {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [{
+          fill: false,
+          // lineTension: 0,
+          backgroundColor: "rgba(0,0,255,1.0)",
+          borderColor: "rgba(0,0,255,1.0)",
+          borderWidth: 8,
+          data: yValues
+        }]
+      },
+      options: {
+        legend: {
+            display: false,
+        },
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        reverse: true
+                    },
+                    stacked: true
+                }
+            ],
+            xAxes: [
+                {
+                    stacked: true
+                }
+            ],
+        },
+        elements: {
+            point: {
+                radius: 0
+            }
+        }
+      }
+    });
+</script>
 
 @endsection
 
 @section('container')
 <div class="container">
 	<br>
-	<div class="text-center">
+
+	<h3 class="text-center"> Build Hold Drop </h3><br>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="inputArea">
+                <form method="GET" action="">
+                    <label for="kop">Kick of Point (V1):</label><br />
+                    <input type="number" step="any" id="kop" name="kop" class="form-control-custom" onkeypress="nextfield('target')" required value="{{ $request->get('kop') }}" /> ft<br />
+                    <label for="target">End of Drop (Target):</label><br />
+                    <input type="number" step="any" id="target" name="target" class="form-control-custom" onkeypress="nextfield('n')" required value="{{ $request->get('target') }}" /> ft<br />
+                    <label for="n">Northing:</label><br />
+                    <input type="number" step="any" id="n" name="n" class="form-control-custom" onkeypress="nextfield('e')"  required value="{{ $request->get('n') }}" /> ft<br />
+                    <label for="e">Easting:</label><br />
+                    <input type="number" step="any" id="e" name="e" class="form-control-custom" onkeypress="nextfield('bur')" required value="{{ $request->get('e') }}"/> ft<br />
+                    <label for="bur">Build Up Rate (BUR):</label><br />
+                    <input type="number" step="any" id="bur" name="bur" class="form-control-custom" onkeypress="nextfield('dor')" required value="{{ $request->get('bur') }}"/> deg/100ft
+                    <label for="dor">Drop Off Rate (DOR):</label><br />
+                    <input type="number" step="any" id="dor" name="dor" class="form-control-custom" onkeypress="nextfield('calculate')" required value="{{ $request->get('dor') }}"/> deg/100ft
+                    <br><br>
+                    <button type="submit" class="btn btn-primary" id="calculate"> <i class="fa fa-calculator"></i> Calculate </button>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="grafikArea">
+                <canvas id="myChart" style="width:100%; max-width:700px;"></canvas>
+            </div>
+        </div>
+    </div>
+
+    <div class="output-area">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="endOfBuild">
+                    <br />
+                    <h3 class="text-center">End of Build</h3>
+                    <table class="table table-striped" id="eob-table">
+                    <tr>
+                        <th>MD</th>
+                        <td>{{ round($eob_md, 4) }}</td>
+                    </tr>
+                    <tr>
+                        <th>VD</th>
+                        <td>{{ round($eob_vd, 4) }}</td>
+                    </tr>
+                    <tr>
+                        <th>Displacement</th>
+                        <td>{{ round($eob_displacement, 4) }}</td>
+                    </tr>
+                    </table>
+                    <br />
+
+                    <h3 class="text-center">Start of Drop</h3>
+                    <table class="table table-striped" id="target-table">
+                    <tr>
+                        <th>MD</th>
+                        <td>{{ round($sod_md, 4) }}</td>
+                    </tr>
+                    <tr>
+                        <th>VD</th>
+                        <td>{{ round($sod_vd, 4) }}</td>
+                    </tr>
+                    <tr>
+                        <th>Displacement</th>
+                        <td>{{ round($sod_displacement, 4) }}</td>
+                    </tr>
+                    </table>
+                    <br />
+
+                    <h3 class="text-center">End of Drop</h3>
+                    <table class="table table-striped" id="target-table">
+                    <tr>
+                        <th>MD</th>
+                        <td>{{ round($eod_md, 4) }}</td>
+                    </tr>
+                    <tr>
+                        <th>VD</th>
+                        <td>{{ round($eod_vd, 4) }}</td>
+                    </tr>
+                    <tr>
+                        <th>Displacement</th>
+                        <td>{{ round($eod_displacement, 4) }}</td>
+                    </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="tabelKedalaman">
+                    <br />
+                    <h3 class="text-center">Depth Table</h3>
+                    <table class="table table-striped" id="depth-table">
+                    <tr>
+                        <th class="text-center">MD (ft) </th>
+                        <th class="text-center">Inclination (deg)</th>
+                        <th class="text-center">TVD (ft) </th>
+                        <th class="text-center">Total Departure (ft)</th>
+                        <th class="text-center">Status</th>
+                    </tr>
+                    @foreach($depth as $row)
+                        <tr>
+                            <td class="text-center">{{ round($row['md'], 3) }}</td>
+                            <td class="text-center">{{ round($row['inclination'], 6) }}</td>
+                            <td class="text-center">{{ round($row['tvd'], 3) }}</td>
+                            <td class="text-center">{{ round($row['total_departure'], 6) }}</td>
+                            <td class="text-center">{{ $row['status'] }}</td>
+                        </tr>
+                    @endforeach
+                    </table>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+	<!-- <div class="text-center">
 		<img src="{{ asset('underconstruction.png') }}">
 		<br>
 		<h3 class="center">Mohon maaf, halaman ini masih dalam tahap pengembangan.</h3>
-	</div>
+	</div> -->
 </div>
 @endsection
