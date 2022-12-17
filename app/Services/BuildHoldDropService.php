273f49b2d9691bf499c7bc6f3652ae733a2c03bd
burhan mafazi <burhanmafazi@gmail.com>
2022-12-17 22:39:03 +0700

    update services

--- app/Services/BuildHoldDropService.php
+++ app/Services/BuildHoldDropService.php
@@ -1,8 +1,297 @@
 <?php 
 
-namespace App\Services\BuildHoldDropService;
+namespace App\Services;
+
+use Illuminate\Http\Request;
 
 class BuildHoldDropService
 {
-	
+	public function calculate(Request $request)
+	{
+		$returnValue = [];
+
+		$depth = [];
+        $chart = [];
+
+        $mdChartValue = [];
+        $tvdChartValue = [];
+
+        $eob_md = 0;
+        $eob_vd = 0;
+        $eob_displacement = 0;
+
+        $sod_md = 0;
+        $sod_vd = 0;
+        $sod_displacement = 0;
+
+        $eod_md = 0;
+        $eod_vd = 0;
+        $eod_displacement = 0;
+
+        $kop = $request->input('kop'); // kick off point
+        $bur = $request->input('bur'); // build up rate
+        $dor = $request->input('dor'); // drop off rate
+
+        $target = $request->input('target'); // target end off drop
+        $n = $request->input('n'); // northing
+        $e = $request->input('e'); // easting
+
+        if ($kop || $bur || $dor || $target || $n || $e) {
+            $pi = pi();
+
+            try {
+                $horizontal_displacement = pow((pow($n, 2) + pow($e, 2)), 0.5);
+                $radius_curvature_bur = (180 * 100) / ($bur * $pi);
+                $radius_curvature_dor = (180 * 100) / ($dor * $pi);
+
+                $sudut_B = 0;
+                $line_OF = 0;
+                $line_OG = 0;
+                $sudut_FOG = 0;
+                $max_inclination = 0;
+
+                $eod_vd = (double) $target;
+
+                if (($radius_curvature_bur + $radius_curvature_dor) < $horizontal_displacement) {
+                    $line_x = $horizontal_displacement - $radius_curvature_bur - $radius_curvature_dor;
+                } else {
+                    $line_x = $radius_curvature_bur - $horizontal_displacement + $radius_curvature_dor;
+                }
+
+                $sudut_B = rad2deg(atan($line_x / ($target - $kop)));
+                $line_OF = ($target - $kop) / cos(deg2rad($sudut_B));
+                $line_OG = pow(pow($line_OF, 2) - pow(($radius_curvature_bur + $radius_curvature_dor), 2), 0.5);
+                $sudut_FOG = rad2deg(asin(($radius_curvature_bur + $radius_curvature_dor) / $line_OF));
+
+                if (($radius_curvature_bur + $radius_curvature_dor) < $horizontal_displacement) {
+                    $max_inclination = $sudut_FOG + $sudut_B;
+                } else {
+                    $max_inclination = $sudut_FOG - $sudut_B;;
+                }
+
+                // eob
+                $eob_md = $kop + $max_inclination / ($bur / 100);;
+                $eob_vd = $kop + $radius_curvature_bur * sin(deg2rad($max_inclination));
+                $eob_displacement = $radius_curvature_bur * (1 - cos(deg2rad($max_inclination)));
+
+                // sod
+                $sod_md = $eob_md + $line_OG;
+                $sod_vd = $eob_vd + $line_OG * cos(deg2rad($max_inclination));
+                $sod_displacement = $eob_displacement + $line_OG * sin(deg2rad($max_inclination));
+
+                // eod
+                $eod_md = $sod_md + $max_inclination / ($dor / 100);
+                $eod_displacement = $horizontal_displacement;
+
+                $inc = 1;
+                $inclination = 0;
+
+                // vertical
+                for ($i=0; $i <= $kop; $i+=100) {
+                    if ($i == $kop) {
+                        $status = 'KOP';
+                    } else {
+                        $status = 'Vertical';
+                    }
+
+                    $total_departure = 0;
+
+                    $depth[$inc]['md'] = $i;
+                    $depth[$inc]['inclination'] = $inclination;
+                    $depth[$inc]['tvd'] = $i;
+                    $depth[$inc]['total_departure'] = $total_departure;
+                    $depth[$inc]['status'] = $status;
+
+                    $inc++;
+                }
+
+                $tvdKOP = $i-100;
+
+                $mdChartValue[] = $total_departure;
+                $tvdChartValue[] = $i-100;
+
+                // build
+                for ($i = $i; $i <= $eob_md; $i+=100) {
+                    $status = 'Build';
+
+                    // find inclination
+                    if ($i > $kop && $eob_md > $i) {
+                        $inclination = $bur + $inclination;
+                    }
+
+                    // find tvd
+                    $tvd = $radius_curvature_bur * sin(deg2rad($inclination)) + $tvdKOP;
+
+                    // find total_departure
+                    $total_departure = $radius_curvature_bur * (1 - cos(deg2rad($inclination)));
+
+                    $depth[$inc]['md'] = $i;
+                    $depth[$inc]['inclination'] = $inclination;
+                    $depth[$inc]['tvd'] = $tvd;
+                    $depth[$inc]['total_departure'] = $total_departure;
+                    $depth[$inc]['status'] = $status;
+
+                    $mdChartValue[] = round($total_departure, 2);
+                    $tvdChartValue[] = $tvd;
+
+                    $inc++;
+                }
+
+                // end of build
+                $status = 'End of Build';
+                $inclinationEOB = ($eob_md - ($i - 100)) * ($bur / 100) + $inclination;
+                $tvdEOB = ($radius_curvature_bur * sin(deg2rad($inclinationEOB))) + $tvdKOP;
+                $total_departureEOB = $radius_curvature_bur * (1 - cos(deg2rad($inclinationEOB)));
+
+                $depth[$inc+1]['md'] = $eob_md;
+                $depth[$inc+1]['inclination'] = $inclinationEOB;
+                $depth[$inc+1]['tvd'] = $tvdEOB;
+                $depth[$inc+1]['total_departure'] = $total_departureEOB;
+                $depth[$inc+1]['status'] = $status;
+
+                $mdChartValue[] = round($total_departureEOB, 2);
+                $tvdChartValue[] = $tvdEOB;
+
+                $inc++;
+
+                // hold
+                for ($i = $i; $i <= $sod_md; $i+=100) {
+                    $first = $i;
+                    $status = 'Hold';
+
+                    // find tvd
+                    // if ($i == $first) {
+                        $tvd = (cos(deg2rad($inclinationEOB)) * ($i - $eob_md)) + $tvdEOB;
+                    // } else {
+                    //     $tvd = (cos(deg2rad($inclinationEOB)) * ($i - ($i - 100))) + $tvd;
+                    // }
+
+                    // find total_departure
+                    $total_departure = $total_departureEOB + (sin(deg2rad($inclinationEOB)) * ($i - $eob_md));
+
+                    $depth[$inc+1]['md'] = $i;
+                    $depth[$inc+1]['inclination'] = $inclinationEOB;
+                    $depth[$inc+1]['tvd'] = $tvd;
+                    $depth[$inc+1]['total_departure'] = $total_departure;
+                    $depth[$inc+1]['status'] = $status;
+
+                    $mdChartValue[] = round($total_departure, 2);
+                    $tvdChartValue[] = $tvd;
+
+                    $inc++;
+                }
+
+                // start of drop
+                $status = 'Start of Drop';
+
+                $tvdSOD = $tvd = (cos(deg2rad($inclinationEOB)) * ($sod_md - ($i - 100))) + $tvd;
+                $total_departureSOD = $horizontal_displacement - ($radius_curvature_dor * (1 - cos(deg2rad($max_inclination))));
+
+                $depth[$inc+1]['md'] = $sod_md;
+                $depth[$inc+1]['inclination'] = $inclinationEOB;
+                $depth[$inc+1]['tvd'] = $tvdSOD;
+                $depth[$inc+1]['total_departure'] = $total_departureSOD;
+                $depth[$inc+1]['status'] = $status;
+
+                $mdChartValue[] = round($total_departureSOD, 2);
+                $tvdChartValue[] = $tvdSOD;
+
+                $inc++;
+
+                // drop
+                for ($i = $i; $i <= $eod_md; $i+=100) {
+                    $first = $i;
+                    $status = 'Drop';
+
+                    // if ($i == $first) {
+                        $inclination = $inclinationEOB - (($i - $sod_md) * ($dor / 100));
+                        $tvd = $sod_vd + ($radius_curvature_dor * (sin(deg2rad($max_inclination)) - sin(deg2rad($inclination))));
+                    // } else {
+                    //     $inclination = '';
+                    //     $tvd = (cos(deg2rad($inclinationEOB)) * ($i - ($i - 100))) + $tvd;
+                    // }
+
+                    // find total_departure
+                    $total_departure = $total_departureSOD + ($radius_curvature_dor * (cos(deg2rad($inclination)) - cos(deg2rad($max_inclination))));
+
+                    $depth[$inc+1]['md'] = $i;
+                    $depth[$inc+1]['inclination'] = $inclination;
+                    $depth[$inc+1]['tvd'] = $tvd;
+                    $depth[$inc+1]['total_departure'] = $total_departure;
+                    $depth[$inc+1]['status'] = $status;
+
+                    $mdChartValue[] = round($total_departure, 2);
+                    $tvdChartValue[] = $tvd;
+
+                    $inc++;
+                }
+
+                // target
+                $status = 'Target';
+
+                $inclination = $inclination - (($eod_md - ($i - 100)) * ($dor / 100));
+                $tvd = $sod_vd + ($radius_curvature_dor * (sin(deg2rad($max_inclination)) - sin(deg2rad($inclination))));
+                $total_departure = $total_departureSOD + ($radius_curvature_dor * (cos(deg2rad($inclination)) - cos(deg2rad($max_inclination))));
+
+                $depth[$inc+1]['md'] = $eod_md;
+                $depth[$inc+1]['inclination'] = abs($inclination);
+                $depth[$inc+1]['tvd'] = $tvd;
+                $depth[$inc+1]['total_departure'] = $total_departure;
+                $depth[$inc+1]['status'] = $status;
+
+                $mdChartValue[] = round($total_departure, 2);
+                $tvdChartValue[] = $tvd;
+
+                // 3d chart
+                for ($i=0; $i < count((array) $mdChartValue); $i++) {
+                    $chart[] = [
+                        'x' => (string) $mdChartValue[$i],
+                        'y' => (string) $tvdChartValue[$i],
+                        'z' => '0',
+                        'color' => '1'
+                    ];
+                }
+            } catch (\Exception $ex) {
+                return redirect()->back()->with('error', $this->errorMessage);
+            } catch (\Throwable $err) {
+                return redirect()->route('build.hold.drop', 
+                    [
+                        'bur' => 0, 
+                        'kop' => 0, 
+                        'dor' => 0, 
+                        'target' => 0, 
+                        'n' => 0, 
+                        'e' => 0
+                    ])->with('error', $this->errorMessage);
+            }
+        } else {
+            $depth[0]['md'] = 0;
+            $depth[0]['inclination'] = 0;
+            $depth[0]['tvd'] = 0;
+            $depth[0]['total_departure'] = 0;
+            $depth[0]['status'] = '-';
+        }
+
+        $returnValue = [
+        	'kop' => $kop,
+        	'bur' => $bur,
+        	'dor' => $dor,
+        	'target' => $target,
+        	'n' => $n,
+        	'e' => $e,
+        	'depth' => $depth,
+        	'chart' => $chart,
+        	'eob_md' => $eob_md,
+        	'eob_vd' => $eob_vd,
+        	'eob_displacement' => $eob_displacement,
+        	'sod_md' => $sod_md,
+        	'sod_vd' => $sod_vd,
+        	'sod_displacement' => $sod_displacement,
+        	'eod_md' => $eod_md,
+        	'eod_vd' => $eod_vd,
+        	'eod_displacement' => $eod_displacement,
+        ];
+
+		return $returnValue;
+	}
 }
--- app/Services/HorizontalWellService.php
+++ app/Services/HorizontalWellService.php
@@ -1,8 +1,179 @@
 <?php 
 
-namespace App\Services\HorizontalWellService;
+namespace App\Services;
+
+use Illuminate\Http\Request;
 
 class HorizontalWellService
 {
-	
+	public function calculate(Request $request)
+	{
+		$returnValue = [];
+
+		$depth = [];
+        $chart = [];
+
+        $mdChartValue = [];
+        $tvdChartValue = [];
+
+        $kop = $request->input('kop'); // kick off point
+        $target = $request->input('target'); // target
+        $e = $request->input('e'); // east
+
+        $horizontal_displacement = 0;
+        $rb = 0;
+        $l = 0;
+        $md = 0;
+        $bur = 0;
+        $eob = 0;
+        $n = 0;
+
+        if ($kop || $target || $e) {
+            $pi = pi();
+            
+            try {
+                $horizontal_displacement = sqrt((pow(0, 2) + pow($e, 2)));
+                $rb = $target - $kop;
+                $l = (90/180) * $pi * $rb;
+                $md = $kop + $l + ($horizontal_displacement - $rb);
+                $bur = (180 / $pi) * (1 / $rb) * 100;
+                $eob = $kop + $l;
+
+                $inc = 1;
+                $inclination = 0;
+                $horizontal_departure = 0;
+
+                for ($i=0; $i <= $md; $i+=100) {
+                    $depth[$inc]['md'] = $i;
+
+                    // inclination
+                    if ($i > $kop && $i < $eob) {
+                        $inclination = $bur + $inclination;
+                    } else {
+                        // kop
+                        if ($i-100 == $kop) {
+                            $inclination = $bur + $inclination;
+                        } else {
+                            if ($i == $eob || $i > $eob) {
+                                $inclination = $inclination;
+                            } else {
+                                // vertical
+                                $inclination = 0;
+                            }
+                        }
+                    }
+
+                    $depth[$inc]['inclination'] = $inclination;
+
+                    // tvd
+                    if ($inclination == 0) {
+                        $tvd = $i;
+                    } else {
+                        $tvd = $kop + $rb * sin(deg2rad($inclination));
+                    }
+
+                    $depth[$inc]['tvd'] = $tvd;
+
+                    // horizontal departure                    
+                    if ($inclination == 0) {
+                        $horizontal_departure = 0;
+                    } else {
+                        if ($i == $eob || $i > $eob) {
+                            $horizontal_departure = $horizontal_departure + 100;
+                        } else {
+                            $horizontal_departure = $rb * (1 - cos(deg2rad($inclination)));
+                        }
+                    }
+
+                    $depth[$inc]['horizontal_departure'] = $horizontal_departure;
+
+                    // status
+                    if ($i == $md) {
+                        $status = 'Target';
+                    } else {
+                        if ($tvd == $kop) {
+                            $status = 'KOP';
+                        } else {
+                            if ($i > $eob) {
+                                $status = 'Hold';
+                            } else {
+                                if (abs($i - $eob) < 100) {
+                                    $status = 'End of Build';
+                                } else {
+                                    if ($horizontal_departure == 0) {
+                                        $status = 'Vertical';
+                                    } else {
+                                        if ($i > $eob) {
+                                            $status = 'Hold';
+                                        } else {
+                                            $status = 'Build';
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    $depth[$inc]['status'] = $status;
+
+                    $mdChartValue[] = round($horizontal_departure, 2);
+                    $tvdChartValue[] = $tvd;
+
+                    $inc++;
+                }
+
+                // target
+                $depth[$inc]['md'] = $md;
+                $depth[$inc]['inclination'] = $inclination;
+                $depth[$inc]['tvd'] = $tvd;
+                $depth[$inc]['horizontal_departure'] = $horizontal_departure + 100;
+                $depth[$inc]['status'] = 'Target';
+
+                $mdChartValue[] = round($horizontal_departure + 100, 2);
+                $tvdChartValue[] = $tvd;
+
+                // 3d chart
+                for ($i=0; $i < count((array) $mdChartValue); $i++) {
+                    $chart[] = [
+                        'x' => (string) $mdChartValue[$i],
+                        'y' => (string) $tvdChartValue[$i],
+                        'z' => '0',
+                        'color' => '1'
+                    ];
+                }
+            } catch (\Exception $ex) {
+               return redirect()->back()->with('error', $this->errorMessage);
+            } catch (\Throwable $err) {
+                return redirect()->route('horizontal.well', 
+                    [
+                        'kop' => 0, 
+                        'target' => 0, 
+                        'e' => 0
+                    ])->with('error', $this->errorMessage);
+            }
+        } else {
+            $depth[0]['md'] = 0;
+            $depth[0]['inclination'] = 0;
+            $depth[0]['tvd'] = 0;
+            $depth[0]['horizontal_departure'] = 0;
+            $depth[0]['status'] = '-';
+        }
+
+        $returnValue = [
+        	'depth' => $depth,
+        	'chart' => $chart,
+        	'kop' => $kop,
+        	'target' => $target,
+        	'e' => $e,
+        	'horizontal_displacement' => $horizontal_displacement,
+        	'rb' => $rb,
+        	'l' => $l,
+        	'md' => $md,
+        	'bur' => $bur,
+        	'eob' => $eob,
+        	'n' => $n
+        ];
+
+		return $returnValue;
+	}
 }
